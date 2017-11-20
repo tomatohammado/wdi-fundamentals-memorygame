3fa378aefdaabf91b3e71660a399603dde4957be
Hammad Malik <hmd.node@gmail.com>
2017-11-19 22:40:39 -0500

    moved most of the code to the new function flipCard, also didn't realize I had redundant code from an earlier exercise so I got rid of that too.

--- js/main.js
+++ js/main.js
@@ -1,29 +1,23 @@
 console.log("Up and running!")
 
-var cardOne = "queen"
-var cardTwo = "queen"
-var cardThree = "king"
-var cardFour = "king"
-
 var cards = ["queen", "queen", "king", "king"]
 var cardsInPlay = []
 
 var flipCard = function (cardId) {
+
+  var cardOne = cards[0]
+  var cardTwo = cards[1]
+  var cardThree = cards[2]
+  var cardFour = cards[3]
   
-}
-
-var cardOne = cards[0]
-var cardTwo = cards[1]
-var cardThree = cards[2]
-var cardFour = cards[3]
-
-cardsInPlay.push(cardOne)
-cardsInPlay.push(cardThree)
-
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!")
-  } else {
-    alert("Sorry, try again.")
+  cardsInPlay.push(cardOne)
+  cardsInPlay.push(cardThree)
+  
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!")
+    } else {
+      alert("Sorry, try again.")
+    }
   }
 }
