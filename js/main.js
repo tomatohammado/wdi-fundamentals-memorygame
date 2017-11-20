e7a0736eb6d6265811e51a96f6dff2a4ffb1dae3
Hammad Malik <hmd.node@gmail.com>
2017-11-20 18:19:22 -0500

    created a createBoard function, however the event handler for flipping over the cards when a user clicks on them doesn't work. I need to figure out how to pass the argument to the function, because it has a parameter.

--- js/main.js
+++ js/main.js
@@ -44,5 +44,17 @@ var flipCard = function (cardId) {
   checkForMatch()
 }
 
-flipCard(0)
-flipCard(2)
+var createBoard = function () {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img')
+    cardElement.setAttribute('src', 'images/back.png')
+    cardElement.setAttribute('data-id', i)
+    cardElement.addEventListener('click', flipCard)
+    document.getElementById('game-board').appendChild(cardElement)
+  }
+}
+
+// flipCard(0)
+// flipCard(2)
+
+createBoard()
