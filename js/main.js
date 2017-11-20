4d5e698482e55655e770a42ae57fd16b62310063
Hammad Malik <hmd.node@gmail.com>
2017-11-19 22:53:37 -0500

    created checkForMatch function and call it, but it runs for each time the flipCard function is called, even when there aren't two cards to compare in cardsInPlay. This means that the first time it runs, when there is only one card, the conditional statement in checkForMatch is false so it prints that there isn't a match. So that's not right.

--- js/main.js
+++ js/main.js
@@ -3,19 +3,21 @@ console.log("Up and running!")
 var cards = ["queen", "queen", "king", "king"]
 var cardsInPlay = []
 
+var checkForMatch = function () {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!")
+  } else {
+    console.log("Sorry, try again.")
+  }
+}
+
 var flipCard = function (cardId) {
 
   console.log("User flipped "+ cards[cardId])
 
   cardsInPlay.push(cards[cardId])
   
-  if (cardsInPlay.length === 2) {
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-      alert("You found a match!")
-    } else {
-      alert("Sorry, try again.")
-    }
-  }
+  checkForMatch()
 }
 
 flipCard(0)
