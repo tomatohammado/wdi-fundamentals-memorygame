4354d360eb745e920ea93f61dee10d4cf84d00fb
Hammad Malik <hmd.node@gmail.com>
2017-11-20 20:53:57 -0500

    refactored js so you can't flip the same card more than once.

--- js/main.js
+++ js/main.js
@@ -25,25 +25,30 @@ var cards = [
 var cardsInPlay = []
 
 var checkForMatch = function () {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
+  if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
     console.log('You found a match!')
+    // window.alert('You found a match!')
   } else {
     console.log('Sorry, try again.')
+    // window.alert('Sorry, try again.')
   }
 }
 
 var flipCard = function () {
+  var cardId = this.getAttribute('data-id')
   if (cardsInPlay.length >= 2) {
     return
+  } else if (cardsInPlay.length === 1 && cardsInPlay[0].cardImage === this.getAttribute('src')) {
+    console.log('Cannot choose the same card twice')
+    return
   }
-
-  var cardId = this.getAttribute('data-id')
-
+  this.classList.add('flipped')
   console.log('User flipped ' + cards[cardId].rank)
   // console.log(cards[cardId].cardImage)
   // console.log(cards[cardId].suit)
 
-  cardsInPlay.push(cards[cardId].rank)
+  // cardsInPlay.push(cards[cardId].rank)
+  cardsInPlay.push(cards[cardId])
 
   this.setAttribute('src', cards[cardId].cardImage)
 
@@ -78,6 +83,16 @@ var createBoard = function () {
     document.getElementById('game-board').insertBefore(cardElement, document.querySelector('#game-board div'))
   }
 }
+
+document.querySelector('.reset-button').addEventListener('click', function () {
+  console.log(document.querySelectorAll('.flipped'))
+  for (var i = 0; i < cardsInPlay.length; i++) {
+    console.log(document.querySelectorAll('.flipped')[i])
+    document.querySelectorAll('.flipped')[i].classList.remove('flipped')
+  }
+
+  cardsInPlay = []
+})
 
 // flipCard(0)
 // flipCard(2)
