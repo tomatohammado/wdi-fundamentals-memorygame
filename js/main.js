8ad0670a8cc776f4b34556352f94d36155ff855b
Hammad Malik <hmd.node@gmail.com>
2017-11-22 17:24:48 -0500

    added code for knuth shuffle, to suffle the contents of an array

--- js/main.js
+++ js/main.js
@@ -25,6 +25,27 @@ var cards = [
 
 var scoreValue, scoreGoal
 
+/* how to shuffle an array, Knuth Shuffle, via https://stackoverflow.com/a/2450976 */
+var shuffle = function (arr) {
+  var currentIndex = arr.length
+  var temporaryValue
+  var randomIndex
+
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
+
+    // And swap it with the current element.
+    temporaryValue = arr[currentIndex]
+    arr[currentIndex] = arr[randomIndex]
+    arr[randomIndex] = temporaryValue
+  }
+
+  return arr
+}
+
 var getSelectedCards = function () {
   return document.querySelectorAll('.selected')
 }
